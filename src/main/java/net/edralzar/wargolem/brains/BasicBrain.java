a9dd1fc78fd312bed77961e51f02b37cbe4573f4
Simon Baslé <simon.basle@gmail.com>
2014-08-12 15:50:08 +0200

    basicBrain logs the room as json when saving

--- src/main/java/net/edralzar/wargolem/brains/BasicBrain.java
+++ src/main/java/net/edralzar/wargolem/brains/BasicBrain.java
@@ -1,14 +1,25 @@
 package net.edralzar.wargolem.brains;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import net.edralzar.wargolem.Brain;
 import net.edralzar.wargolem.model.Player;
 import net.edralzar.wargolem.model.WarRoom;
 
 public class BasicBrain implements Brain {
 
+    private final ObjectMapper jsonMapper = new ObjectMapper();
+
     @Override
     public void save(WarRoom room) {
         //NO-OP
+        try {
+            System.out.println(jsonMapper.writeValueAsString(room));
+            System.out.println();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
